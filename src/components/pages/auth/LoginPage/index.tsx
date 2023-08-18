5b5cbd46b0ee24f0eed134ccac74878027a2f1a2
Benjamin Johnson <ben332004@gmail.com>
2023-08-17 23:15:07 -0700

    Log response headers for debugging

--- src/components/pages/auth/LoginPage/index.tsx
+++ src/components/pages/auth/LoginPage/index.tsx
@@ -43,6 +43,7 @@ export const LoginPage = () => {
       backendClient
         .post(url, postData, options)
         .then((response) => {
+          console.log(response.headers);
           Cookies.set('csrftoken', response.headers['X-Csrftoken']);
           setCsrfTokenHeader(response.headers['X-Csrftoken']);
           refreshUser();
